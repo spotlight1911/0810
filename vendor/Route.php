e891dde2171e7b29628855a22d62f70f28568bdd
edorofeev89 <85448490+edorofeev89@users.noreply.github.com>
2021-10-08 19:40:14 +0300

    .htaccess created and method init

--- vendor/Route.php
+++ vendor/Route.php
@@ -4,6 +4,28 @@
 class Route
 {
     static public function init(){
-        echo 'Hello world!!!';
+        $controllerName = 'index';
+        $actionName = 'index';
+        $url = $_SERVER['REDIRECT_URL']??'';
+        $url = ltrim($url, '/');
+        $urlComponents = explode('/', $url);
+        if(count($urlComponents)>2){
+            exit('no page');//TODO
+        }
+        if(!empty($urlComponents[0])){
+            $controllerName = strtolower($urlComponents[0]);
+        }
+        if(!empty($urlComponents[1])){
+            $actionName = strtolower($urlComponents[1]);
+        }
+        $controllerClass = 'controllers\\'.ucfirst($controllerName);
+        if(!class_exists($controllerClass)){
+            exit('no class');//TODO
+        }
+        $controller = new $controllerClass();
+        if(!method_exists($controller, $actionName)){
+            exit('no method');//TODO
+        }
+        $controller->$actionName();
     }
 }
